b658c8f6d6a19b403009b1af71e702f0af130dc6
iamshaunjp <shaun@thenetninja.co.uk>
2024-04-25 12:11:34 +0100

    lesson-4

--- lib/main.dart
+++ lib/main.dart
@@ -14,7 +14,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.blue),
         useMaterial3: true,
       ),
       home: const WelcomeScreen()
--- lib/screens/welcome/welcome.dart
+++ lib/screens/welcome/welcome.dart
@@ -2,6 +2,7 @@ import 'package:flutter_auth_tut/screens/welcome/sign_in.dart';
 import 'package:flutter_auth_tut/screens/welcome/sign_up.dart';
 import 'package:flutter_auth_tut/shared/styled_text.dart';
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class WelcomeScreen extends StatefulWidget {
   const WelcomeScreen({super.key});
@@ -11,6 +12,8 @@ class WelcomeScreen extends StatefulWidget {
 }
 
 class _WelcomeScreenState extends State<WelcomeScreen> {
+  bool isSignUpForm = true;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -22,16 +25,44 @@ class _WelcomeScreenState extends State<WelcomeScreen> {
       body: SingleChildScrollView(
         child: Container(
           padding: const EdgeInsets.symmetric(vertical: 48, horizontal: 16),
-          child: const Column(
+          child: Column(
             crossAxisAlignment: CrossAxisAlignment.center,
             children: [
-              StyledHeading('Welcome.'),
+              const StyledHeading('Welcome.'),
         
               // sign up screen
-              SignInForm(),
+              if (isSignUpForm)
+                Column(
+                  children: [
+                    const SignUpForm(),
+                    const StyledBodyText('Already have an account?'),
+                    TextButton(
+                      onPressed: () {
+                        setState(() {
+                          isSignUpForm = false;
+                        });
+                      },
+                      child: Text('Sign in instead', style: GoogleFonts.poppins()),
+                    )
+                  ]
+                ),
 
               // sign in screen
-              SignUpForm(),
+              if (!isSignUpForm)
+                Column(
+                  children: [
+                    const SignInForm(),
+                    const StyledBodyText('Need an account?'),
+                    TextButton(
+                      onPressed: () {
+                        setState(() {
+                          isSignUpForm = true;
+                        });
+                      },
+                      child: Text('Sign up instead', style: GoogleFonts.poppins()),
+                    )
+                  ]
+                ),
             ]
           )
         ),
