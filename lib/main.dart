88b6d0e9d982e856d7bc2d44fc8aef318d07016e
iamshaunjp <shaun@thenetninja.co.uk>
2024-04-26 15:37:33 +0100

    lesson-6

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,17 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_auth_tut/screens/welcome/welcome.dart';
 
-void main() {
+// firebase
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
   runApp(const MyApp());
 }
 
