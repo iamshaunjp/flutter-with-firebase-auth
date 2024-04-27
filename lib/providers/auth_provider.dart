b4b8a25e02e88b193b34b75c87e3537bcc97a3fd
iamshaunjp <shaun@thenetninja.co.uk>
2024-04-27 12:07:27 +0100

    lesson-13

--- lib/providers/auth_provider.dart
+++ lib/providers/auth_provider.dart
@@ -1,10 +1,20 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_auth_tut/models/app_user.dart';
 
 final authProvider = StreamProvider.autoDispose<AppUser?>((ref) async* {
   
   // create a stream provides continues values (user/null)
-
-  // YIELD that value whenevr it changes
+  final Stream<AppUser?> userStream = FirebaseAuth.instance.authStateChanges().map((user) {
+    if (user != null) {
+      return AppUser(uid: user.uid, email: user.email!);
+    }
+    return null;
+  });
+  
+  // YIELD that value whenever it changes
+  await for (final user in userStream) {
+    yield user;
+  }   
 
 });
